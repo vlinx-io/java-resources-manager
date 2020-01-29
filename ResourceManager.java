a4aeac358d84ac4521f1dd8d2b6d1ac8ab93912d
vlinx <vlinx@vlinx.io>
2020-01-29 20:20:50 +0800

    Create ResourceManager.java

---
+++ ResourceManager.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class ResourceManager {
+
+    public String getResourcePath(String relativePath) {
+        try {
+            return URLDecoder.decode(getBasePath() + File.separator + relativePath, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return getBasePath() + File.separator + relativePath;
+        }
+    }
+
+    public String getBasePath() {
+        File file = new File(ResourceManager.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+
+        String absPath = "";
+
+        if (file.isDirectory()) {
+            absPath = file.getAbsolutePath();
+        } else {
+            absPath = file.getParentFile().getAbsolutePath();
+        }
+
+        try {
+            return URLDecoder.decode(absPath, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return absPath;
+        }
+    }
+
+}
