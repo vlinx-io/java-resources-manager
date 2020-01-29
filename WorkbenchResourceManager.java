68a309eb8b561e20ef67bd11efc5e13072765a93
vlinx <vlinx@vlinx.io>
2020-01-29 20:24:54 +0800

    Create WorkbenchResourceManager.java

---
+++ WorkbenchResourceManager.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+/*
+ * Resource manager of Eclipse workspace
+ */
+public class WorkbenchResourceManager {
+
+	@Override
+	public String getBasePath() {
+		String absPath = Activator.getDefault().getStateLocation().makeAbsolute().toFile().getAbsolutePath();
+		return absPath;
+	}
+
+	@Override
+	public String getResourcePath(String relativePath) {
+		String basePath = getBasePath();
+
+		File file = new File(basePath);
+		if (file.isDirectory()) {
+			String absPath = file.getAbsolutePath() + File.separator + relativePath;
+			return absPath;
+		} else {
+			if (file.getParent() == null) {
+				return new File("").getAbsolutePath() + File.separator + relativePath;
+			}
+			return file.getParent() + File.separator + relativePath;
+		}
+	}
+}
